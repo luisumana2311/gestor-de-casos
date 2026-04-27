56099e404cce8ec5e8c629319ddd3f1de2b08296
Mariano Umana <mymuplift@gmail.com>
2026-04-26 23:09:25 -0600

    arreglo de auth

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const { login, registrarUsuario } = require("../controllers/authController");
-const verificarToken = require("../middlewares/authMiddleware");
+const verificarToken = require("../middleware/authMiddleware");
 
 // Público
 router.post("/login", login);
